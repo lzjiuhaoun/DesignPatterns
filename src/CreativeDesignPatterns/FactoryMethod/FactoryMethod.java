bf3c6310118fdd0e4588ed2f9c4d990eac41aab3
LiuZhaoJun <1811176067@qq.com>
2024-04-13 14:05:54 +0800

    update 创建型设计模式-工厂方法

--- src/CreativeDesignPatterns/FactoryMethod/FactoryMethod.java
+++ src/CreativeDesignPatterns/FactoryMethod/FactoryMethod.java
@@ -4,15 +4,19 @@ package CreativeDesignPatterns.FactoryMethod;
  * ClassName: FactoryMethod
  * Package: CreativeDesignPatterns
  * Description: 创建型设计模式-工厂方法
+ *  工厂方法：使一个类的实例化延迟到其子类
  *
  * @Author 工学院-liuzhaojun
  * @Create 2024/4/13 13:44
  */
 public class FactoryMethod {
     public static void main(String[] args) {
-        Product productA = new FactoryA().createProduct();
+        //使一个类的实例化延迟到其子类
+        Factory factoryA = new FactoryA();
+        Product productA = factoryA.createProduct();
         productA.info();
-        Product productB = new FactoryB().createProduct();
+        Factory factoryB = new FactoryB();
+        Product productB = factoryB.createProduct();
         productB.info();
 
     }
@@ -30,7 +34,6 @@ class FactoryA implements Factory {
 }
 
 class FactoryB implements Factory {
-
     @Override
     public Product createProduct() {
         return new ProductB();
